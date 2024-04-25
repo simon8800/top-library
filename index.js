6ef8dc134459c45b997d71e047b785acd5b11a43
Simon Mei <smei8800@gmail.com>
2024-04-25 11:16:19 -0400

    Add dummy book data

--- index.js
+++ index.js
@@ -12,55 +12,16 @@ function addBook(title, author, pages, read) {
   console.log(myLibrary);
 }
 
+const books = [
+  {title: 'The Women: A Novel', author: 'Kristin Hannah', pages: 480, read: false},
+  {title: 'The Familiar', author: 'Leigh Bardugo', pages: 880, read: false},
+  {title: 'Fourth Wing', author: 'Rebecca Yarros', pages: 528, read: false},
+  {title: 'Toxic Prey', author: 'John Sandford', pages: 400, read: false},
+]
+
 const myLibrary = [];
 const addBookEntryBtn = document.querySelector(".add-btn");
 console.log(addBookEntryBtn);
 addBookEntryBtn.onclick = function () {
   console.log("Add new book clicked!");
-  let formContainer = document.querySelector(".form-container");
-
-  // form creation
-  let form = document.createElement("form");
-
-  // inputs
-  let titleInput = document.createElement("input");
-  titleInput.type = "text";
-  titleInput.placeholder = "Enter title";
-  titleInput.id = "title";
-  let titleLabel = document.createElement("label");
-  titleLabel.htmlFor = "title";
-  titleLabel.textContent = "Title:";
-
-  let authorInput = document.createElement("input");
-  authorInput.type = "text";
-  authorInput.placeholder = "Enter author";
-  authorInput.id = "author";
-  let authorLabel = document.createElement("label");
-  authorLabel.htmlFor = "author";
-  authorLabel.textContent = "Author:";
-
-  let pageInput = document.createElement("input");
-  pageInput.type = "number";
-  pageInput.placeholder = "Enter pages";
-  pageInput.id = "page";
-  let pageLabel = document.createElement("label");
-  pageLabel.htmlFor = "page";
-  pageLabel.textContent = "Pages:";
-
-  let readInput = document.createElement("input");
-  readInput.type = "checkbox";
-  readInput.id = "read";
-  let readLabel = document.createElement("label");
-  readLabel.htmlFor = "read";
-  readLabel.textContent = "Read:";
-
-  form.appendChild(titleLabel);
-  form.appendChild(titleInput);
-  form.appendChild(authorLabel);
-  form.appendChild(authorInput);
-  form.appendChild(pageLabel);
-  form.appendChild(pageInput);
-  form.appendChild(readLabel);
-  form.appendChild(readInput);
-  formContainer.appendChild(form);
 };
